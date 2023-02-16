3646e0e9881aa1409a4531d90987be3f1eb91f13
Tushar Maharanagit <tusharhero@sdf.org>
2023-02-16 18:40:14 +0530

    initial commit

---
+++ entab.c
@@ -0,0 +1,3 @@
+//A program entab that replaces strings of blanks by the minimum number of tabs and blanks to acheive the same spacing.
+//Use the same tab stops as for detab. 
+//When either a tab or a single blank would suffice to reach a tab stop, which should be given preference?

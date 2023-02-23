2c79b436c9c95d146e08e2f009bf1ed9040a1521
Tushar Maharanagit <tusharhero@sdf.org>
2023-02-23 16:38:17 +0530

    add some.more comments

--- entab.c
+++ entab.c
@@ -11,11 +11,11 @@ void entab(char line[MAXSIZE],char entabbedline[MAXSIZE],int len){
   int i,j; i = j = 0;
   int consecutive_spaces = 0;
   while(i < len && j < len){
-    if(line[i] == ' '){
+    if(line[i] == ' '){// count the number of consecutive spaces
       consecutive_spaces++;
       ++i;
     }
-    if(consecutive_spaces >= TABSTOPS){
+    if(consecutive_spaces >= TABSTOPS){//if consecutive spaces are greater than the number of TABSTOPS then replace with tab
       j -= consecutive_spaces;
       printf("hello I am being executed\n");
       entabbedline[j] = 'l';
@@ -26,6 +26,7 @@ void entab(char line[MAXSIZE],char entabbedline[MAXSIZE],int len){
       entabbedline[j] = line[i];
       ++j; ++i;
     }
+    printf("(%d,%d,%d)\n",i,j,consecutive_spaces);
   }
   entabbedline[j] = 0;
 }
